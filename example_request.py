2f133c0020e13007b5cf473af0e428249ae74330
Yüsra Tekin <tekinyusra66@gmail.com>
2024-04-05 11:23:00 +0300

    Pthon Request

---
+++ example_request.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+
+#GET
+#user_input = input("Enter id: ")
+#get_url = f"https://jsonplaceholder.typicode.com/todos{user_input}"
+
+#get_response = requests.get(get_url)
+#print(get_response.json())
+
+#POST
+to_do_item ={"userId": 2, "title": "my to do", "completed": False}
+post_url = "https://jsonplaceholder.typicode.com/todos"
+
+#optional hearder
+headers = {"Content-Type":"application/json"}
+#post_response = requests.post(post_url,json=to_do_item,headers=headers)
+post_response = requests.post(post_url, data=json.dumps(to_do_item) , headers=headers)
+print(post_response.json())
---
+++ putpatch_example.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+#GET
+get_url = "https://jsonplaceholder.typicode.com/todos/15"
+
+#get_response = requests.get(get_url)
+#print(get_response.json())
+
+
+#PUT
+to_do_item_15 = {"userId": 2, "title": "put title", "completed": False}
+put_response = requests.put(get_url, json=to_do_item_15)
+print(put_response.json())
+
+
+#PATCH
+#to_do_item_patch_15 = {"title": "Patch Test"}
+#patch_response = requests.patch(get_url, json=to_do_item_patch_15)
+#print(patch_response.json())
+
+#DELETE
+#delete_reponse = requests.delete(get_url)
+#print(delete_reponse.json())
+#print(delete_reponse.status_code)
